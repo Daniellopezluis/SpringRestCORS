bfb0bcbab0ea088ac58ca39915540c0990d385b9
Daniel Lopez <daniellopezluis@gmail.com>
2016-01-25 12:40:05 +0100

    Initial commit. Fully functioning project.

---
+++ src/main/java/com/daniel/restfulcors/GreetingController.java
@@ -0,0 +1,40 @@
+package com.daniel.restfulcors;
+
+import java.util.Random;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+//------ CONTROLADORES ------//
+@Controller
+public class GreetingController {
+
+    private static final String template = " : %s!";
+
+    @CrossOrigin(origins = "http://localhost:9000")
+    @RequestMapping("/greeting")
+    public @ResponseBody Greeting greeting(@RequestParam(required=false, defaultValue="La eterna brevedad del ser") String name) {
+        System.out.println("==== saludando ====");
+        int x = randInt(0,5);
+        return new Greeting(x, String.format(template, generarFrase(x)));
+    }
+
+ //------ FUNCIONES PARA GENERAR EL CONTENIDO DEL JSON ------//    
+    public static int randInt(int min, int max) {
+        Random rand = new Random();
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+        System.out.println("Numero rand: " + randomNum);
+        return randomNum;
+    }
+    
+    public String generarFrase(int x){
+    	String[] frases = {"Erase una vez", "Happy new year", "What is love?","En el bosque de la china","Alakazam"};
+    	return frases[x];
+    }
+
+}
