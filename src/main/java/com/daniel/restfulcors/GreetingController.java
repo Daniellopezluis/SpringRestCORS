7c33bf741ae3c2a71b0fb1d7a5988725ba630d8d
Daniel Lopez <daniellopezluis@gmail.com>
2016-01-25 13:06:03 +0100

    Additional changes. More strings to choose from. Best code practices.

--- src/main/java/com/daniel/restfulcors/GreetingController.java
+++ src/main/java/com/daniel/restfulcors/GreetingController.java
@@ -11,14 +11,16 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 //------ CONTROLADORES ------//
 @Controller
 public class GreetingController {
-
+	
+	// formato para mostrar la frase
     private static final String template = " : %s";
 
+    // Soporte CORS. Necesario?
     @CrossOrigin(origins = "http://localhost:9000")
     @RequestMapping("/greeting")
     public @ResponseBody Greeting greeting(@RequestParam(required=false, defaultValue="La eterna brevedad del ser") String name) {
         System.out.println("==== saludando ====");
-        int x = randInt(0,4);
+        int x = randInt(0,7);
         return new Greeting(x, String.format(template, generarFrase(x)));
     }
 
@@ -31,11 +33,15 @@ public class GreetingController {
     }
     
     public String generarFrase(int x){
-    	String[] frases = {"All my life I thouth air was free... until I bought a bag of chips", 
-    			"Happy new year", 
-    			"What is love?",
-    			"En el bosque de la china",
-    			"Alakazam"};
+    	String[] frases = {"All my life I thouth air was free... until I bought a bag of chips.", 
+    			"When I die, I want to go peacefully like my grandfather did: in his sleep. "
+    			+ "Not yelling and screaming like the passengers in his car.", 
+    			"Always borrow money from a pessimist. He won’t expect it back.",
+    			"Friendship is like peeing on yourself: everyone can see it, but only you get the warm feeling that it brings.",
+    			"Dogs have masters. Cats have staff.",
+    			"Knowledge is knowing a tomato is a fruit; wisdom is not putting it in a fruit salad.",
+    			"I love deadlines. I like the whooshing sound they make as they fly by.",
+    			"The best way to lie is to tell the truth . . . carefully edited truth."};
     	return frases[x];
     }
 
